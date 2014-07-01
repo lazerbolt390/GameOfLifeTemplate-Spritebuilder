2d3839f9fb52f77068a0a3e684fd97461ceec8bf
Manish R <Lazerbolt@Manishs-MacBook-Pro.local>
2014-07-01 11:08:49 +0800

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -85,4 +85,17 @@ static const int GRID_COLUMNS = 10;
  
 }
 
+
+- (void) evolveStep {
+    //update each Creature's neighbor count
+  //  [self countNeighbors];
+    
+    //update each Creature's state
+  //  [self updateCreatures];
+    
+    //update the generation so the label's text will display the correct generation
+    _generation++;
+    
+}
+
 @end
