00eac2621ac497c28faeb1a4df1fdbb9d496f4fc
Manish R <Lazerbolt@Manishs-MacBook-Pro.local>
2014-07-01 10:49:05 +0800

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -73,7 +73,13 @@ static const int GRID_COLUMNS = 10;
 }
 - (Creature *)creatureForTouchPosition:(CGPoint)touchPosition
 {
-    //get the row and column that was touched, return the Creature inside the corresponding cell int row = touchPosition.y / _cellHeight; int collumn = touchPosition.x / _cellWidth; return _gridArray[row][collumn]; }
+    //get the row and column that was touched, return the Creature inside the corresponding cell
+    int row = touchPosition.y/_cellHeight;
+    int column = touchPosition.x/_cellWidth;
+    
+    
+    return _gridArray[row][column];
+
 }
 
 
